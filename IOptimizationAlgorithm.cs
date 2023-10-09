5f891359dea8a4026acad1a3e27f92556e54987e
MrTheoslav <wo303232@student.polsl.pl>
2023-10-09 10:32:19 +0200

    Utworzenie interfejsu

---
+++ IOptimizationAlgorithm.cs
@@ -0,0 +1,12 @@
+﻿public interface IOptimizationAlgorithm
+{
+    string HarrisHawksOptimization { get; set; }
+
+    double Solve();
+
+    double[] XBest { get; set; }
+
+    double FBest { get; set; }
+
+    int NumberOfEvaluationFitnessFunction { get; set; }
+}
---
+++ Zastosowanie metod sztucznej inteligencji - projekt 1/IOptimizationAlgorithm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Zastosowanie_metod_sztucznej_inteligencji___projekt_1
+{
+
+    public interface IOptimizationAlgorithm
+    {
+        string HarrisHawksOptimization { get; set; }
+
+        double Solve();
+
+        double[] XBest { get; set; }
+
+        double FBest { get; set; }
+
+        int NumberOfEvaluationFitnessFunction { get; set; }
+    }
+}
