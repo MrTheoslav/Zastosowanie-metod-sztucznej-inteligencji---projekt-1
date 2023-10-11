28d4c09b92971b5f6c483e314b2a8f397603b0da
Emilia Pawela <ep303242@student.polsl.pl>
2023-10-12 01:20:38 +0200

    Add TestFunction

---
+++ Zastosowanie metod sztucznej inteligencji - projekt 1/HarrisHawksOptimization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zastosowanie_metod_sztucznej_inteligencji___projekt_1
+{
+    public class HarrisHawksOptimization : IOptimizationAlgorithm
+    {
+        public double[] XBest { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double FBest { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int NumberOfEvaluationFitnessFunction { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string IOptimizationAlgorithm.HarrisHawksOptimization { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public double Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
