167a1c3b23377fdfba7b0824f632e3d238b3475c
MrTheoslav <wo303232@student.polsl.pl>
2023-10-24 23:14:16 +0200

    Save to CSV File

---
+++ Zastosowanie metod sztucznej inteligencji - projekt 1/SaveToCSV.cs
@@ -0,0 +1,31 @@
+﻿using CsvHelper;
+using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zastosowanie_metod_sztucznej_inteligencji___projekt_1
+{
+    public class SaveToCSV
+    {
+        public void Save(string filePath, List<TableOfResults> table)
+        {
+            try
+            {
+                using (var writer = new StreamWriter(filePath))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(table);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+            }
+        }
+    }
+}
---
+++ Zastosowanie metod sztucznej inteligencji - projekt 1/TableOfResults.cs
@@ -0,0 +1,18 @@
+﻿namespace Zastosowanie_metod_sztucznej_inteligencji___projekt_1
+{
+    public class TableOfResults
+    {
+        public string Algorithm { get; set; }
+        public string TestFunction { get; set; }
+        public int NumberOfParameters { get; set; }
+        public double Parameters { get; set; }
+        public int Iterator { get; set; }
+        public int Size { get; set; }
+        public string Minimum { get; set; }
+        public string StandartDeviationForParameters { get; set; }
+        public string VariationCoefficientForParameter {  get; set; }
+        public string ObjectiveFunction { get; set; }
+        public string StandartDeviationForFunction { get; set; }
+        public string VariationCoefficientForFunction { get;set; }
+    }
+}
